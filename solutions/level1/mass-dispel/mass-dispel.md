c7fd7a26b1ff413fc8566fd2c6ebf15615c258cb
Roman Grewal <grps.hq@gmail.com>
2025-10-03 03:46:32 -0400

    Mass Dispel

---
+++ solutions/level1/mass-dispel/mass-dispel.md
@@ -0,0 +1,7 @@
+## Mass Dispel 
+### Challenge: Close multiple "Challenge solved"-notifications in one go.
+
+* Restart Juice Shop instance
+* Delete browser cookies
+* Solve multiple challenges
+* Shift-click on any "Challenge solved" notification to close all open notifications of this kind
--- solutions/level1/mass-dispel/solution.py
+++ solutions/level1/mass-dispel/solution.py
@@ -1,16 +1,19 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
 
 try:
 	driver = webdriver.Chrome()
 	driver.get("http://192.168.0.217:3000")
-
-	#User inputs malicious JavaScript into search box
 	WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//button[contains(., 'Dismiss')]"))).click()
+	
+	#User navigates to score board
+	driver.get("http://192.168.0.217:3000/#/score-board")
+	
+	#User inputs malicious JavaScript into search box
 	WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "searchQuery"))).click()
 	WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "mat-input-1"))).send_keys("<iframe src=\"javascript:alert('xss')\">")
 	WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "mat-input-1"))).send_keys(Keys.ENTER)
@@ -20,6 +23,12 @@ try:
 	alert = driver.switch_to.alert
 	# To accept the alert (click 'OK')
 	alert.accept()
+
+	#User shift clicks to close all "Challenge solved" notifications	
+	WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "closeButton")))
+	closeButtons = driver.find_elements(By.ID, "closeButton")
+	actions = ActionChains(driver)
+	actions.key_down(Keys.SHIFT).click(closeButtons[0]).key_up(Keys.SHIFT).perform()
 	
 finally:
   driver.quit()
