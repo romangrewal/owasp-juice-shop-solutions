5ce264eb16c22e058dd4adbc520e078e719478df
Roman Grewal <grps.hq@gmail.com>
2025-10-03 18:29:56 -0400

    missing encoding

---
+++ solutions/level1/missing-encoding/missing-encoding.md
@@ -0,0 +1,8 @@
+## Missing Encoding
+### Challenge: Retrieve the photo of Bjoern's cat in "melee combat-mode".
+
+* Visit the Photo Wall
+* Inspect src to find the broken image
+* Right click on the image src to visit in new tab
+* Observe that image does not render because of '#' encoding
+* Encode '#' to '%23' in the URl to render the image onto the screen
---
+++ solutions/level1/missing-encoding/solution.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+
+try:
+	driver = webdriver.Chrome()
+	driver.get("http://192.168.0.217:3000")
+
+	#User navigates to Photo Wall
+	WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//button[contains(., 'Dismiss')]"))).click()
+	WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//button[contains(., 'menu')]"))).click()
+	WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//a[contains(., 'Photo Wall')]"))).click()
+
+	#User inspects src to find broken image url
+	alt_text = "😼 #zatschi #whoneedsfourlegs"
+	broken_image_element = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, f"//img[@alt='{alt_text}']")))
+	broken_src = broken_image_element.get_attribute("src")
+
+	#User visits absolute url with copy-pasted image url
+	base_domain = "http://192.168.0.217:3000/"
+	broken_full_url = base_domain + broken_src
+	driver.get(broken_full_url)
+
+	#User fixes encoding to reveal image
+	corrected_relative_path = broken_src.replace("#", "%23")
+	corrected_full_url = base_domain + corrected_relative_path
+	driver.get(corrected_full_url)	
+
+finally:
+  driver.quit()
