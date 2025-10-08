9ce1599a0eb83fac35dbbbbc1c07c7c54955aeae
Roman Grewal <grps.hq@gmail.com>
2025-10-08 09:04:25 -0400

    clean merge

--- solutions/level1/web3-sandbox/solution.py
+++ solutions/level1/web3-sandbox/solution.py
@@ -31,22 +31,22 @@ try:
 	IP_ADDRESS = os.environ['OWASP_JUICE_SHOP_IP_ADDRESS']
 	PORT = os.environ['OWASP_JUICE_SHOP_PORT']
 	search_string = input("Enter search route: ")
-  #Fetch HTML content (user opens browser)
+	#Fetch HTML content (user opens browser)
 	url = f"http://{IP_ADDRESS}:{PORT}"
 	response = requests.get(url)
 	html_content = response.text
 
 	driver = webdriver.Chrome()
-  #Parse HTML for JS files (user searches for JS files)
+	#Parse HTML for JS files (user searches for JS files)
 	soup = BeautifulSoup(html_content, 'html.parser')
 	js_srcs = [script['src'] for script in soup.find_all('script') if 'src' in script.attrs]
 
-  #Create absolute URLs of all JS files
+	#Create absolute URLs of all JS files
 	absolute_client_files = []
 	for file_path in js_srcs:
 		absolute_client_files.append(urljoin(url, file_path))
 
-  #Parse all JS files for search_string and append to URL if found (user searches JS files for URL route & tests route in browser)
+	#Parse all JS files for search_string and append to URL if found (user searches JS files for URL route & tests route in browser)
 	for file_url in absolute_client_files:
 		found, content = scan_js_for_string(file_url, search_string)
 
