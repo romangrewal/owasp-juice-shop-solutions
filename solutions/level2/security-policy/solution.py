b370ef183f852b76c8b5c92fe62febd47e6d3622
Roman Grewal <grps.hq@gmail.com>
2025-11-17 02:06:08 -0500

    Update solution.py

--- solutions/level2/security-policy/solution.py
+++ solutions/level2/security-policy/solution.py
@@ -1,5 +1,4 @@
 from selenium import webdriver
-#from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -9,8 +8,7 @@ try:
 	IP_ADDRESS = os.environ['OWASP_JUICE_SHOP_IP_ADDRESS']
 	PORT = os.environ['OWASP_JUICE_SHOP_PORT']
 
-  #User opens browser to security disclosure page
-	driver = webdriver.Chrome(options=chrome_options)
+  	#User opens browser to security disclosure page
 	driver.get(f"http://{IP_ADDRESS}:{PORT}/.well-known/security.txt")
 
 	input("Press Enter...")
